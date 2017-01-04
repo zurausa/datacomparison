42b48452a8189d4c9346b7cacfe2190bbaffd4bd
zurausa <zurausa_77@yahoo.co.jp>
2017-01-05 02:55:43 +0900

    first commit
    create project

---
+++ src/datacomparison/core.clj
@@ -0,0 +1,15 @@
+(ns datacomparison.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+
+(defn add-two-sheets-and-save
+    "シートとセルの追加"
+        ( [ ] (add-two-sheets-and-save "workbook-01.xlsx"))
+        ([fname:
+        (with-open
+  )
---
+++ test/datacomparison/core_test.clj
@@ -0,0 +1,7 @@
+(ns datacomparison.core-test
+  (:require [clojure.test :refer :all]
+            [datacomparison.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
